ac10bc1cbe0d7ddecce67dda5c5a6b6d163c8d10
alekseisanatov <alexsanatov@gmail.com>
2026-04-23 11:51:39 +0300

    feat: add queryClient and queryProvider in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { HomePage } from "./pages/HomePage";
 import { ProductPage } from "./pages/ProductPage";
 import { NotFoundPage } from "./pages/NotFoundPage";
 import { Header } from "./components/Header";
 
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            retry: 2,
+            refetchOnWindowFocus: false,
+        },
+    },
+});
+
 export const App = () => {
     return (
-        <BrowserRouter>
-            <Header />
-            <main>
-                <Routes>
-                    <Route
-                        path="/"
-                        element={<Navigate to="/devices" replace />}
-                    />
-                    <Route path="/devices" element={<HomePage />} />
-                    <Route path="/devices/:id" element={<ProductPage />} />
-                    <Route path="*" element={<NotFoundPage />} />
-                </Routes>
-            </main>
-        </BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+                <Header />
+                <main>
+                    <Routes>
+                        <Route
+                            path="/"
+                            element={<Navigate to="/devices" replace />}
+                        />
+                        <Route path="/devices" element={<HomePage />} />
+                        <Route path="/devices/:id" element={<ProductPage />} />
+                        <Route path="*" element={<NotFoundPage />} />
+                    </Routes>
+                </main>
+            </BrowserRouter>
+        </QueryClientProvider>
     );
 };
