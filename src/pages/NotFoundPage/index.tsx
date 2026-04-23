8a210a52a69807e12dd31b670a5afa6c6ef72808
alekseisanatov <alexsanatov@gmail.com>
2026-04-23 14:42:23 +0300

    feat: add notFound page

--- src/pages/NotFoundPage/index.tsx
+++ src/pages/NotFoundPage/index.tsx
@@ -1,3 +1,16 @@
+import { Link } from "react-router-dom";
+import styles from "./NotFoundPage.module.css";
+
 export const NotFoundPage = () => {
-  return <></>;
+  return (
+    <div className={styles.wrapper}>
+      <h1 className={styles.code}>404</h1>
+      <p className={styles.message}>
+        The page you were looking for doesn't exist.
+      </p>
+      <Link to="/devices" className={styles.link}>
+        Back to devices
+      </Link>
+    </div>
+  );
 };
