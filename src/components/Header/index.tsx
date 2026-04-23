3e68558fe413e3f1a5b103e736604784bf2434ac
alekseisanatov <alexsanatov@gmail.com>
2026-04-23 14:14:04 +0300

    feat: add header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,14 @@
 import logo from "../../assets/logo.png";
+import styles from "./Header.module.css";
 
 export const Header = () => {
-  return (
-    <header>
-      <img src={logo} alt="Ubiquiti" />
-    </header>
-  );
+    return (
+        <header className={styles.header}>
+            <div className={styles.left}>
+                <img src={logo} alt="Ubiquiti" className={styles.logo} />
+                <span className={styles.title}>Devices</span>
+            </div>
+            <span className={styles.author}>Aleksejs Sanatovs</span>
+        </header>
+    );
 };
