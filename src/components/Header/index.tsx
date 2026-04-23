5818bf99c9a368dd6bf0c42dcacf072b6d3c14f7
alekseisanatov <alexsanatov@gmail.com>
2026-04-23 11:21:38 +0300

    feat: add initial folders and files

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import logo from "../../assets/logo.png";
+
+export const Header = () => {
+  return (
+    <header>
+      <img src={logo} alt="Ubiquiti" />
+    </header>
+  );
+};
---
+++ src/lib/api.ts
@@ -0,0 +1,3 @@
+const UIDB_URL = "https://static.ui.com/fingerprint/ui/public.json";
+
+export const fetchDevices = async () => {};
